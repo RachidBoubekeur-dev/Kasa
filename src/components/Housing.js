122978bff9f13855268a734c05a3cdfccb7fa1f0
Rachid Boubekeur <rachidboubekeur42100@gmail.com>
2021-07-01 00:03:48 +0200

    🔨🚧 work develoment script for component Housing.js

--- src/components/Housing.js
+++ src/components/Housing.js
@@ -1,8 +1,9 @@
 import { Component } from 'react';
 import Error from './Error';
 import PropTypes from 'prop-types';
-import previous from '../assets/previous.png';
 import '../styles/Housing.css';
+import chevron from '../assets/chevron.png';
+import star from '../assets/star.png';
 
 export default class Housing extends Component {
     render() {
@@ -11,7 +12,30 @@ export default class Housing extends Component {
         const houseData = housingsData.filter((house) => {
             return house.id === id;
         });
-        window.document.title = `Kasa - ${houseData[0].title}`;
+
+        const setTitle = (title) => {
+            window.document.title = `Kasa - ${title}`;
+        };
+
+        const getRating = (rating) => {
+            let arrayRating = [];
+            for (let i = 1; i < 6; i++) {
+                let starStyle;
+                if (i > parseInt(rating)) starStyle = 'starGray';
+                else starStyle = 'starRed';
+                arrayRating.push(
+                    <span key={i}>
+                        <img
+                            className={starStyle}
+                            src={star}
+                            alt="Note du logement"
+                        />
+                    </span>
+                );
+            }
+            return arrayRating;
+        };
+
         return (
             <main className="mainHousing">
                 <section>
@@ -21,12 +45,12 @@ export default class Housing extends Component {
                                 <div className="slidePictures">
                                     <img
                                         className="previous"
-                                        src={previous}
+                                        src={chevron}
                                         alt="Previous"
                                     />
                                     <img
                                         className="next"
-                                        src={previous}
+                                        src={chevron}
                                         alt="Next"
                                     />
                                     <div>
@@ -36,6 +60,56 @@ export default class Housing extends Component {
                                         />
                                     </div>
                                 </div>
+                                <h2>{house.title}</h2>
+                                <h3>{house.location}</h3>
+                                <div className="divTags">
+                                    {house.tags.map((tag, index) => (
+                                        <span key={index}>{tag}</span>
+                                    ))}
+                                </div>
+                                <div className="divRatingAndHost">
+                                    <div className="divRating">
+                                        {getRating(house.rating)}
+                                    </div>
+                                    <div className="divHost">
+                                        <span>
+                                            {house.host.name.split(' ')[0]}
+                                            <br />
+                                            {house.host.name.split(' ')[1]}
+                                        </span>
+                                        <img
+                                            src={house.host.picture}
+                                            alt={house.host.name}
+                                        />
+                                    </div>
+                                </div>
+                                <div className="divDetails">
+                                    <div className="divDescription">
+                                        <p>
+                                            Description
+                                            <img src={chevron} alt="Déployer" />
+                                        </p>
+                                        <div>
+                                            <p>{house.description}</p>
+                                        </div>
+                                    </div>
+                                    <div className="divEquipements">
+                                        <p>
+                                            Équipements
+                                            <img src={chevron} alt="Réduire" />
+                                        </p>
+                                        <div>
+                                            {house.equipments.map(
+                                                (equipment, index) => (
+                                                    <p key={index}>
+                                                        {equipment}
+                                                    </p>
+                                                )
+                                            )}
+                                        </div>
+                                    </div>
+                                </div>
+                                {setTitle(house.title)}
                             </article>
                         ))
                     ) : (
